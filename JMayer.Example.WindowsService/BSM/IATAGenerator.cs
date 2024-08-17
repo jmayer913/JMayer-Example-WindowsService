1620e31bd87e3d4316a334c8a493abcc157d0d78
jmayer913 <72579603+jmayer913@users.noreply.github.com>
2024-08-17 10:59:41 -0400

    Added IATA Generator & Unit Test

---
+++ JMayer.Example.WindowsService/BSM/IATAGenerator.cs
@@ -0,0 +1,53 @@
+﻿namespace JMayer.Example.WindowsService.BSM;
+
+/// <summary>
+/// The class manages generating an IATA (10 digit tag number).
+/// </summary>
+public class IATAGenerator
+{
+    /// <summary>
+    /// Keeps track of the sequence number to be used.
+    /// </summary>
+    private int _sequenceNumber = MinSequenceNumber;
+
+    /// <summary>
+    /// The property gets/sets the alphanumeric  airline code.
+    /// </summary>
+    /// <remarks>
+    /// Used by the BSM generator.
+    /// </remarks>
+    public string AirlineAlphaNumericCode { get; init; } = string.Empty;
+
+    /// <summary>
+    /// The property gets/sets the numeric airline code used by the IATA. 
+    /// </summary>
+    public string AirlineNumericCode { get; init; } = string.Empty;
+
+    /// <summary>
+    /// The constant for the maximum sequence number.
+    /// </summary>
+    public const int MaxSequenceNumber = 999999;
+
+    /// <summary>
+    /// The constant for the minimum sequence number.
+    /// </summary>
+    public const int MinSequenceNumber = 1;
+
+    /// <summary>
+    /// The method returns the next IATA.
+    /// </summary>
+    /// <returns>The IATA.</returns>
+    public string Generate()
+    {
+        string iata = $"0{AirlineNumericCode}{_sequenceNumber.ToString().PadLeft(6, '0')}";
+
+        _sequenceNumber++;
+
+        if (_sequenceNumber > MaxSequenceNumber)
+        {
+            _sequenceNumber = MinSequenceNumber;
+        }
+
+        return iata;
+    }
+}
---
+++ TestProject/Test/IATAGeneratorUnitTest.cs
@@ -0,0 +1,69 @@
+﻿using JMayer.Example.WindowsService.BSM;
+
+namespace TestProject.Test;
+
+/// <summary>
+/// The class manages testing the IATA generator.
+/// </summary>
+public class IATAGeneratorUnitTest
+{
+    /// <summary>
+    /// The constant for the airline numeric code.
+    /// </summary>
+    private const string AirlineNumericCode = "001";
+
+    /// <summary>
+    /// The method verifies an IATA will be generated.
+    /// </summary>
+    [Fact]
+    public void VerifyGeneration()
+    {
+        IATAGenerator iataGenerator = new()
+        {
+            AirlineNumericCode = AirlineNumericCode,
+        };
+        string iata = iataGenerator.Generate();
+
+        Assert.Equal($"0{iataGenerator.AirlineNumericCode}{IATAGenerator.MinSequenceNumber.ToString().PadLeft(6, '0')}", iata);
+    }
+
+    /// <summary>
+    /// The method verifies if 3 IATAs are generated, each is incremented by 1.
+    /// </summary>
+    [Fact]
+    public void VerifyIncrementByOne()
+    {
+        IATAGenerator iataGenerator = new()
+        {
+            AirlineNumericCode = AirlineNumericCode,
+        };
+        
+        string iata1 = iataGenerator.Generate();
+        string iata2 = iataGenerator.Generate();
+        string iata3 = iataGenerator.Generate();
+
+        Assert.Equal($"0{iataGenerator.AirlineNumericCode}{IATAGenerator.MinSequenceNumber.ToString().PadLeft(6, '0')}", iata1);
+        Assert.Equal($"0{iataGenerator.AirlineNumericCode}{(IATAGenerator.MinSequenceNumber + 1).ToString().PadLeft(6, '0')}", iata2);
+        Assert.Equal($"0{iataGenerator.AirlineNumericCode}{(IATAGenerator.MinSequenceNumber + 2).ToString().PadLeft(6, '0')}", iata3);
+    }
+
+    /// <summary>
+    /// The method verifies the generator will rollover after 999999 IATAs are generated.
+    /// </summary>
+    [Fact]
+    public void VerifyRollover()
+    {
+        string iata = string.Empty;
+        IATAGenerator iataGenerator = new()
+        {
+            AirlineNumericCode = AirlineNumericCode,
+        };
+
+        for (int index = 0; index <= IATAGenerator.MaxSequenceNumber; index++)
+        {
+            iata = iataGenerator.Generate();
+        }
+
+        Assert.Equal($"0{iataGenerator.AirlineNumericCode}{IATAGenerator.MinSequenceNumber.ToString().PadLeft(6, '0')}", iata);
+    }
+}
